9eb8d485cfcaa294c37366b69ccc16da2f73e730
VinayTS <vinay@succorfish.com>
2021-02-10 04:41:11 +0530

    Addding a Fragment Dynamically using a button click is complete.

--- Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/MainActivity.java
+++ Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/MainActivity.java
@@ -4,15 +4,32 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.fragments.codetutor.Fragments.Fragment_one;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_AddFragmet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addFragment();
+        intializeView();
+        onClickSetListner();
+    }
+
+    private void intializeView(){
+        button_AddFragmet=(Button)findViewById(R.id.mainActivtiy_AddButton);
+    }
+
+    private void onClickSetListner(){
+        button_AddFragmet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment();
+            }
+        });
     }
 
     private void addFragment(){
