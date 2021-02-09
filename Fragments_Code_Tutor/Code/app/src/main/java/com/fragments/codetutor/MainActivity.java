c054f524e34b97123acc45277b8c42c4ba31b344
VinayTS <vinay@succorfish.com>
2021-02-10 04:31:20 +0530

    UI changes for the Add Fragment Dynamically.

--- Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/MainActivity.java
+++ Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/MainActivity.java
@@ -1,10 +1,23 @@
 package com.fragments.codetutor;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+
+import com.fragments.codetutor.Fragments.Fragment_one;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void addFragment(){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        Fragment_one fragment_one=new Fragment_one();
+
     }
 }
