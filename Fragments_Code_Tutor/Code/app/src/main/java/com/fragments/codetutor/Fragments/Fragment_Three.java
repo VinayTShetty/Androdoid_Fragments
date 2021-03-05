5acd7f5b237407f7c109de4ec2239edbb5ab134a
VinayTS <vinay@succorfish.com>
2021-03-05 06:32:43 +0530

    Added Logs for checking the Method execution.

--- Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/Fragments/Fragment_Three.java
+++ Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/Fragments/Fragment_Three.java
@@ -2,6 +2,7 @@ package com.fragments.codetutor.Fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,59 +14,73 @@ import com.fragments.codetutor.BaserFragment.BaseFragment;
 import com.fragments.codetutor.R;
 
 public class Fragment_Three extends BaseFragment {
+    public static final String FRAGMENT_NAME=Fragment_Three.class.getSimpleName();
+    public static final String COMBINED_TAG="COMBINED_LIFECYCLE";
+    public static final String TAG=COMBINED_TAG;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        Log.d(TAG, FRAGMENT_NAME+" onAttach: ");
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,FRAGMENT_NAME+" OnCreate ");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG,FRAGMENT_NAME+" onCreateView ");
         return inflater.inflate(R.layout.fragment_three,container,false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG,FRAGMENT_NAME+" onActivityCreated");
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        Log.d(TAG,FRAGMENT_NAME+" onStart");
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(TAG,FRAGMENT_NAME+" onResume");
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Log.d(TAG,FRAGMENT_NAME+" onPause");
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        Log.d(TAG,FRAGMENT_NAME+" onStop");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        Log.d(TAG,FRAGMENT_NAME+" onDestroyView");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        Log.d(TAG,FRAGMENT_NAME+" onDetach");
     }
 
     @Override
     public String toString() {
+        Log.d(TAG,FRAGMENT_NAME+" toString");
         return Fragment_Three.class.getSimpleName().toString();
     }
 }
--- Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/Fragments/SampleFragment.java
+++ Fragments_Code_Tutor/Code/app/src/main/java/com/fragments/codetutor/Fragments/SampleFragment.java
@@ -2,6 +2,7 @@ package com.fragments.codetutor.Fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,61 +12,75 @@ import androidx.annotation.Nullable;
 
 import com.fragments.codetutor.BaserFragment.BaseFragment;
 import com.fragments.codetutor.R;
-
 public class SampleFragment extends BaseFragment {
+    public static final String FRAGMENT_NAME=Fragment_two.class.getSimpleName();
+    public static final String COMBINED_TAG="COMBINED_LIFECYCLE";
+    public static final String TAG=COMBINED_TAG;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
+        Log.d(TAG, FRAGMENT_NAME+" onAttach: ");
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, FRAGMENT_NAME+" onCreate: ");
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, FRAGMENT_NAME+" onCreateView: ");
         return inflater.inflate(R.layout.sample_fragment,container,false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, FRAGMENT_NAME+" onActivityCreated: ");
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        Log.d(TAG, FRAGMENT_NAME+" onStart: ");
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(TAG, FRAGMENT_NAME+" onResume: ");
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        Log.d(TAG, FRAGMENT_NAME+" onPause: ");
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        Log.d(TAG, FRAGMENT_NAME+" onStop: ");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        Log.d(TAG, FRAGMENT_NAME+" onDestroyView: ");
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
+        Log.d(TAG, FRAGMENT_NAME+" onDetach: ");
     }
 
     @Override
     public String toString() {
-        return SampleFragment.class.getSimpleName().toString();
+        Log.d(TAG, FRAGMENT_NAME+" ToString ");
+        return SampleFragment.class.getSimpleName();
     }
 }
+
